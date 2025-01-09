0ffe43c8962577530fac3dbdf374b7b90b4cbf59
ebotsmith2000 <ebotsmith@gmail.com>
2025-01-08 22:57:13 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import ReactGA from "react-ga4";
+
+
+ReactGA.initialize("G-63CPK0BKQB");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
